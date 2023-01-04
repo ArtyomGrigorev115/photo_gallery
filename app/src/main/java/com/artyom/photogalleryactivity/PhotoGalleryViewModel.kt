8da11e17e652546f2c825a2e17a519ebabfb434e
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2023-01-04 22:35:27 +0700

    Создал ViewModel что бы при уничтожении активности не создавать лишних запросов к Flickr

---
+++ app/src/main/java/com/artyom/photogalleryactivity/PhotoGalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.artyom.photogalleryactivity
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+class PhotoGalleryViewModel : ViewModel() {
+
+    val galleryItemLiveData: LiveData<List<GalleryItem>>
+    
+    init {
+        galleryItemLiveData = FlickrFetchr().fetchPhotos()
+    }
+}
