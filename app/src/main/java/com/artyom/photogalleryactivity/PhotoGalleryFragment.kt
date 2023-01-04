94583a0c0e4bc0765dc850fcd47ecb6caa8327be
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2023-01-04 21:15:59 +0700

    репозиторий FlickrFetchr.kt

--- app/src/main/java/com/artyom/photogalleryactivity/PhotoGalleryFragment.kt
+++ app/src/main/java/com/artyom/photogalleryactivity/PhotoGalleryFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.artyom.photogalleryactivity.api.FlickrApi
@@ -23,12 +25,13 @@ class PhotoGalleryFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        val retrofit: Retrofit = Retrofit.Builder().baseUrl("https://www.flickr.com/")
+
+      /*  val retrofit: Retrofit = Retrofit.Builder().baseUrl("https://www.flickr.com/")
             .addConverterFactory(ScalarsConverterFactory.create())
             .build()
-        val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
+        val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)*/
 
-        val flickrHomePageRequest: Call<String> = flickrApi.fetchContents()
+      /*  val flickrHomePageRequest: Call<String> = flickrApi.fetchContents()
 
 
         flickrHomePageRequest.enqueue(object : Callback<String> {
@@ -40,7 +43,13 @@ class PhotoGalleryFragment : Fragment() {
             override fun onResponse(call: Call<String>, response: Response<String>) {
                 Log.d(TAG, "Response received: ${response.body()}")
             }
-        })
+        })*/
+        val flickrLiveData: LiveData<String> = FlickrFetchr().fetchContents()
+        flickrLiveData.observe(
+            this,
+            Observer { responseString ->
+                Log.d(TAG, "Response received: $responseString")
+            })
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
